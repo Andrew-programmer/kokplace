1047ef2ee68943780710916f59fef6ce4d9703b7
DanilKrylov <danuha2350@gmail.com>
2023-10-25 20:05:15 +0300

    add user model

---
+++ Movie/Movie/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie.Models
+{
+    public abstract class User
+    {
+        [Key]
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
