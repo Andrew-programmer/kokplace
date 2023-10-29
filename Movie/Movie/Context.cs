2f2b8e1cfabee6391bbe63eb474743e584c34491
DanilKrylov <danuha2350@gmail.com>
2023-10-29 21:09:27 +0200

    add context

---
+++ Movie/Movie/Context.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie.Models;
+using System.Collections.Generic;
+
+namespace Movie
+{
+    public class Context : DbContext
+    {
+        public DbSet<Buyer> Buyers { get; set; }
+
+        public DbSet<Company> Companies { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public Context()
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=movies;Trusted_Connection=True;");
+        }
+    }
+}
