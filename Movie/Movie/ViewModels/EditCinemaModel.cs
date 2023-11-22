732121938d7ac085b52c31b34fbee0be39f367ae
DanilKrylov <danuha2350@gmail.com>
2023-11-22 19:06:48 +0200

    added halls and seats controllers

---
+++ Movie/Movie/ViewModels/EditCinemaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Movie.ViewModels
+{
+    public class EditCinemaModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public IFormFile Logo { get; set; }
+        public string CompanyLogin { get; set; }
+    }
+}
